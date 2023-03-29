0d8c9aa28ccdac53148105aa3ec8ef3b81a866c0
remku <ngancloverr+remkugithub@gmail.com>
2023-03-29 11:30:39 +0530

    New

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,38 +1,39 @@
 "use client";
 
-function GooglePayButton({ amount = "100" }: { amount: string }): JSX.Element {
-  function isGooglePayInstalled(): boolean {
-    const userAgent = navigator.userAgent;
-    return (
-      userAgent.indexOf("Android") > -1 && userAgent.indexOf("Chrome") > -1
-    );
-  }
-
-  function openGooglePay(): void {
-    const packageName = "com.google.android.apps.walletnfcrel";
-    const paymentData = {
-      transactionId: "12345",
-      totalPriceStatus: "FINAL",
-      totalPrice: amount,
-      currencyCode: "USD",
-      merchantId: "YOUR_MERCHANT_ID",
-    };
-    const scheme = `intent://pay/#Intent;scheme=https;package=${packageName};S.payload=${encodeURIComponent(
-      JSON.stringify(paymentData)
-    )};end;`;
-
-    window.location.href = scheme;
-  }
-
-  function handleClick(): void {
-    if (isGooglePayInstalled()) {
-      openGooglePay();
-    } else {
-      alert("Please install the Google Pay app to proceed.");
-    }
-  }
-
-  return <button onClick={handleClick}>Pay with Google Pay</button>;
+function isGooglePayInstalled(): boolean {
+  const userAgent = navigator.userAgent;
+  return userAgent.indexOf("Android") > -1 && userAgent.indexOf("Chrome") > -1;
 }
 
-export default GooglePayButton;
+function openGooglePay(amount: string): void {
+  const packageName = "com.google.android.apps.walletnfcrel";
+  const paymentData = {
+    transactionId: "12345",
+    totalPriceStatus: "FINAL",
+    totalPrice: amount,
+    currencyCode: "INR",
+    merchantId: "YOUR_MERCHANT_ID",
+  };
+  const scheme = `intent://pay/#Intent;scheme=https;package=${packageName};S.payload=${encodeURIComponent(
+    JSON.stringify(paymentData)
+  )};end;`;
+
+  window.location.href = scheme;
+}
+
+function handleClick(amount: string): void {
+  if (isGooglePayInstalled()) {
+    openGooglePay(amount);
+  } else {
+    alert("Please install the Google Pay app to proceed.");
+  }
+}
+
+export default function MyButton() {
+  return (
+    <div>
+      <h1>Nganaremba</h1>
+      <button onClick={() => handleClick("100")}>Pay with Google Pay</button>
+    </div>
+  );
+}
